c2e472d6d3022d3ecf4a2121681d9b721d65aa10
Hugh J. Devore <hughdevore@yahoo.com>
2016-06-19 23:49:33 -0600

    Adding modal changes .

--- app/js/freelancer.js
+++ app/js/freelancer.js
@@ -6,13 +6,17 @@
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('body').on('click', '.page-scroll a', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+    st = $(document).scrollTop();
+
+    if(st >= 700) {
+        $('body').on('click', '.page-scroll a', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    }
 });
 
 // Floating label headings for the contact form
